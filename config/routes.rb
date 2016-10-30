b00b5581758433dfee0e7440efa70fc58d304ff4
Zefanya Putri <zefanyanadia.putri@yahoo.com>
2016-10-30 17:25:13 -0400

    Issue #4: Connect login and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'onboarding/new'
 
   get 'sessions/new'
+    
+  get 'welcome/homepage'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup' => 'users#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018203601) do
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-
 ActiveRecord::Schema.define(version: 20161030055554) do
 
   create_table "groups", force: :cascade do |t|
@@ -28,6 +18,15 @@ ActiveRecord::Schema.define(version: 20161030055554) do
     t.text     "group_members"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
