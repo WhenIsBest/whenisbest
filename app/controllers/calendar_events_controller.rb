a2df2c48745688f9e66ba726f5613ab33c3baeb2
harrij15 <harrij15@rpi.edu>
2016-11-20 04:28:31 -0500

    Fixed time display for event

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -18,7 +18,7 @@ class CalendarEventsController < ApplicationController
     
     def create
       @calendar_event = CalendarEvent.new(calendar_event_params)
-      @calendar_event.host = current_user
+      @calendar_event.host = current_user.email
       puts calendar_event_params
       
       year = calendar_event_params["date(1i)"]
