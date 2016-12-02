0081af85082b703e4ab40c724217355be5d69201
harrij15 <harrij15@rpi.edu>
2016-12-01 19:37:42 -0500

    Added group calendar pt 1 and fixed rows

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   around_action :set_time_zone, if: :current_user
   helper_method :current_user
+  helper_method :current_group
   #before_filter :require_user
     
   def current_user
@@ -12,7 +13,11 @@ class ApplicationController < ActionController::Base
   def require_user
       redirect_to '/' unless current_user
   end
-    
+  
+  def current_group
+      @current_group ||= Group.find(session[:group_id]) if session[:group_id]
+  rescue ActiveRecord::RecordNotFound
+  end
   private
     def set_time_zone(&block)
       Time.use_zone(current_user.time_zone, &block)
