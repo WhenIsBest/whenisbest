f5ece90343e6490c91709bae7bb5d74e17456949
harrij15 <harrij15@rpi.edu>
2017-02-27 18:32:22 -0500

    Database fix attempt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220105508) do
+ActiveRecord::Schema.define(version: 20170227092327) do
 
   create_table "calendar_events", force: :cascade do |t|
     t.string   "host"
