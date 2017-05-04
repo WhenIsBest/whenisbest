7fde2bc15c2876fb4f80e13b03270e6f9ff4ada4
erinjordan24 <erinjordan24@gmail.com>
2017-05-04 06:47:51 -0400

    fixed merge errors

--- db/schema.rb
+++ db/schema.rb
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 20170424083914) do
     t.string   "duration"
   end
 
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "start_time"
-    t.integer  "duration"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "groups", force: :cascade do |t|
     t.text     "name"
     t.text     "description"
